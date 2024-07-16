fa94ee16ced9d1e40619a0a4cc70f1747449f437
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2024-07-17 03:26:06 +0700

    feat(sidebar): add all stasi menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,36 +1,52 @@
 <div class="sidebar-wrapper">
     <div>
-        <div class="logo-wrapper"><a href="index.html"><img class="img-fluid for-light"
-                    src="{{ asset('assets/images/logo/logo.png') }}" alt=""><img class="img-fluid for-dark"
-                    src="{{ asset('assets/images/logo/logo_dark.png') }}" alt=""></a>
+        <div class="logo-wrapper">
+            <a href="javascript:void(0)">
+                <img class="img-fluid for-light" src="{{ asset('assets/images/logo/logo.png') }}" alt="">
+                <img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}" alt="">
+            </a>
             <div class="back-btn"><i class="fa fa-angle-left"></i></div>
             <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="grid"> </i></div>
         </div>
-        <div class="logo-icon-wrapper"><a href="index.html"><img class="img-fluid"
-                    src="{{ asset('assets/images/logo/logo-icon') }}.png" alt=""></a></div>
+        <div class="logo-icon-wrapper">
+            <a href="javascript:void(0)">
+                <img class="img-fluid" src="{{ asset('assets/images/logo/logo-icon') }}.png" alt="">
+            </a>
+        </div>
         <nav class="sidebar-main">
             <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
             <div id="sidebar-menu">
                 <ul class="sidebar-links" id="simple-bar">
-                    <li class="back-btn"><a href="index.html"><img class="img-fluid"
-                                src="{{ asset('assets/images/logo/logo-icon.png') }}" alt=""></a>
-                        <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2"
-                                aria-hidden="true"></i></div>
+                    <li class="back-btn">
+                        <a href="javascript:void(0)">
+                            <img class="img-fluid" src="{{ asset('assets/images/logo/logo-icon.png') }}" alt="">
+                        </a>
+                        <div class="mobile-back text-end">
+                            <span>Back</span>
+                            <i class="fa fa-angle-right ps-2" aria-hidden="true"></i>
+                        </div>
                     </li>
                     <li class="sidebar-main-title">
                         <div>
-                            <h6 class="lan-1">General</h6>
-                            <p class="lan-2">Dashboards,widgets & layout.</p>
+                            <h6 class="lan">Pengelolaan Data</h6>
+                            <p class="lan">Jemaat, Marriage, Terms.</p>
                         </div>
                     </li>
                     <li class="sidebar-list">
-                        <label class="badge badge-success">2</label><a class="sidebar-link sidebar-title"
-                            href="#"><i data-feather="home"></i><span class="lan-3">Dashboard
-                            </span></a>
+                        <a class="sidebar-link sidebar-title" href="#">
+                            <i data-feather="home"></i>
+                            <span class="lan">Pengelolaan Data</span>
+                        </a>
                         <ul class="sidebar-submenu">
-                            <li><a class="lan-4" href="index.html">Default</a></li>
-                            <li><a class="lan-5" href="dashboard-02.html">Ecommerce</a></li>
+                            <li><a class="lan" href="{{ route('getDataJemaat') }}">Jemaat</a></li>
+                            <li><a class="lan" href="javascript:void(0)">Persyaratan Pernikahan</a></li>
                         </ul>
+                    </li>
+                    <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title link-nav" href="javascript:void(0)">
+                            <i data-feather="check-square"></i>
+                            <span>Persetujuan Pernikahan</span>
+                        </a>
                     </li>
                 </ul>
             </div>
