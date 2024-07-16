ed61657f14ad0215e1c047a9549284f3b5fd4018
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2024-07-17 03:26:23 +0700

    feat(auth): remove show password function

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -61,7 +61,6 @@
                                     <div class="form-input position-relative">
                                         <input class="form-control" type="password" name="password"
                                             required="" placeholder="*********">
-                                        <div class="show-hide"><span class="show"> </span></div>
                                     </div>
                                 </div>
                                 <div class="form-group mb-0 mt-4">
